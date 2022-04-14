0a0184944de5e17726d842ed7a0d9e32b444f5e9
Rayyan (r - eye - an) <xfaonae@protonmail.com>
2022-04-14 08:58:15 -0400

    [Docs: terminal.*] Added doc commenting

--- terminal/src/main/java/net/skylix/elixor/terminal/logger/Logger.java
+++ terminal/src/main/java/net/skylix/elixor/terminal/logger/Logger.java
@@ -6,18 +6,36 @@ import java.sql.Timestamp;
  * A class used for printing messages to the terminal.
  */
 public class Logger {
+    /**
+     * The default logger instance.
+     */
     private static Logger baseLogger;
 
+    /**
+     * Settings for the logger and its configuration/
+     */
     private final LoggerSettings settings;
 
+    /**
+     * Create a new logger instance.
+     */
     public Logger() {
         this(new LoggerSettings());
     }
 
+    /**
+     * Create a new logger instance with the given settings.
+     * @param settings Settings to configure the behavior of the logger.
+     */
     public Logger(LoggerSettings settings) {
         this.settings = settings;
     }
 
+    /**
+     * Log a message with a prefix type.
+     * @param message The message to log.
+     * @param type The type of the message.
+     */
     private void logWithPrefix(String message, LoggerTypes type) {
         String padding = " ".repeat(settings.padding);
 
@@ -73,34 +91,66 @@ public class Logger {
         System.out.println((!settings.useTimeStamps ? "" : (timeStamp + padding)) + prefixLabel + padding + message);
     }
 
+    /**
+     * Log an informative message.
+     * @param message The message to log.
+     */
     public void info(String message) {
         logWithPrefix(message, LoggerTypes.INFO);
     }
 
+    /**
+     * Log a message indicating a successful operation.
+     * @param message The message to log.
+     */
     public void success(String message) {
         logWithPrefix(message, LoggerTypes.SUCCESS);
     }
 
+    /**
+     * Log a message warning the user of a potential problem.
+     * @param message The message to log.
+     */
     public void warning(String message) {
         logWithPrefix(message, LoggerTypes.WARNING);
     }
 
+    /**
+     * Log a message indicating an error has occurred.
+     * @param message The message to log.
+     */
     public void error(String message) {
         logWithPrefix(message, LoggerTypes.ERROR);
     }
 
+    /**
+     * Log a message for debug usage.
+     * @param message The message to log.
+     */
     public void debug(String message) {
         logWithPrefix(message, LoggerTypes.DEBUG);
     }
 
+    /**
+     * Log a message for verbose actions.
+     * @param message The message to log.
+     */
     public void verbose(String message) {
         logWithPrefix(message, LoggerTypes.VERBOSE);
     }
 
+    /**
+     * Log a message indicating deprecation of a feature.
+     * @param message The message to log.
+     */
     public void deprecated(String message) {
         logWithPrefix(message, LoggerTypes.DEPRECATED);
     }
 
+    /**
+     * Create and configure the default static logger.
+     * @return The default static logger.
+     */
     private static Logger setupBaseLogger() {
         if (baseLogger == null) {
             baseLogger = new Logger();
@@ -109,30 +159,58 @@ public class Logger {
         return baseLogger;
     }
 
+    /**
+     * Log an informative message with the default logger.
+     * @param message The message to log.
+     */
     public static void infoBase(String message) {
         setupBaseLogger().info(message);
     }
 
+    /**
+     * Log a message indicating a successful operation via the default logger.
+     * @param message The message to log.
+     */
     public static void successBase(String message) {
         setupBaseLogger().success(message);
     }
 
+    /**
+     * Log a message warning the user of a potential problem via the default logger.
+     * @param message The message to log.
+     */
     public static void warningBase(String message) {
         setupBaseLogger().warning(message);
     }
 
+    /**
+     * Log a message indicating an error has occurred via the default logger.
+     * @param message The message to log.
+     */
     public static void errorBase(String message) {
         setupBaseLogger().error(message);
     }
 
+    /**
+     * Log a message for debug usage via the default logger.
+     * @param message The message to log.
+     */
     public static void debugBase(String message) {
         setupBaseLogger().debug(message);
     }
 
+    /**
+     * Log a message for verbose actions via the default logger.
+     * @param message The message to log.
+     */
     public static void verboseBase(String message) {
         setupBaseLogger().verbose(message);
     }
 
+    /**
+     * Log a message indicating deprecation of a feature via the default logger.
+     * @param message The message to log.
+     */
     public static void deprecatedBase(String message) {
         setupBaseLogger().deprecated(message);
     }
--- terminal/src/main/java/net/skylix/elixor/terminal/logger/LoggerSettings.java
+++ terminal/src/main/java/net/skylix/elixor/terminal/logger/LoggerSettings.java
@@ -4,25 +4,58 @@ package net.skylix.elixor.terminal.logger;
  * Settings for the logger.
  */
 public class LoggerSettings {
+    /**
+     * Whether to render a timestamp before every logged message.
+     */
     public boolean useTimeStamps = true;
 
+    /**
+     * Text for informative messages.
+     */
     public String infoText = "info";
 
+    /**
+     * Text for warning messages.
+     */
     public String warningText = "warning";
 
+    /**
+     * Text for error messages.
+     */
     public String errorText = "error";
 
+    /**
+     * Text for debug messages.
+     */
     public String debugText = "debug";
 
+    /**
+     * Text for success messages.
+     */
     public String successText = "success";
 
+    /**
+     * Text for verbose messages.
+     */
     public String verboseText = "verbose";
 
+    /**
+     * Text for deprecated messages.
+     */
     public String deprecatedText = "deprecated";
 
+    /**
+     * Padding between text, surrounds and prefixes.
+     */
     public Integer padding = 1;
 
+    /**
+     * Text to surround the indicator prefix.
+     */
     public String[] prefixTextSurrounds = { "[", "]:" };
 
+    /**
+     * Text that surrounds the time stamps.
+     */
     public String[] prefixTimeSurrounds = { "[", "]" };
 }
