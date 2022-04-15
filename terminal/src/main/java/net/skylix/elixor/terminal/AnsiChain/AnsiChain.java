df29ff5014e4db9b5dc7d181d39050b11fa77978
Rayyan (r - eye - an) <xfaonae@protonmail.com>
2022-04-15 08:22:00 -0400

    [Docs: terminal.ansiChain.AnsiChain] JavaDoc

--- terminal/src/main/java/net/skylix/elixor/terminal/AnsiChain/AnsiChain.java
+++ terminal/src/main/java/net/skylix/elixor/terminal/AnsiChain/AnsiChain.java
@@ -1,46 +1,85 @@
 package net.skylix.elixor.terminal.AnsiChain;
 
-import net.skylix.elixor.terminal.color.ColorConversion;
 import net.skylix.elixor.terminal.color.ColorUtil;
 import net.skylix.elixor.terminal.color.ColorsCLI256;
 import net.skylix.elixor.terminal.color.errors.InvalidHexCode;
 import net.skylix.elixor.terminal.color.errors.InvalidRGBAlpha;
 import net.skylix.elixor.terminal.color.errors.InvalidRGBValues;
 
-import java.util.Arrays;
-
+/**
+ * A class used for modification of text visual to be displayed on the terminal.
+ */
 public class AnsiChain {
+    /**
+     * If the text should render in bold.
+     */
     private boolean bold = false;
 
+    /**
+     * If the text should render with an underline.
+     */
     private boolean underline = false;
 
+    /**
+     * The color ID for the background.
+     */
     private Integer bgColor = null;
 
+    /**
+     * The color ID for the foreground text.
+     */
     private Integer fgColor = null;
 
+    /**
+     * If the text should render with italics.
+     */
     private boolean italic = false;
 
+    /**
+     * Set the bold rendering state of the text.
+     * @param boldEnabled If the text should render in bold.
+     */
     public void bold(boolean boldEnabled) {
         bold = boldEnabled;
     }
 
+    /**
+     * Enable bold text.
+     */
     public void bold() {
         bold(true);
     }
 
+    /**
+     * Set the underline rendering state of the text.
+     * @param underlineEnabled If the text should render with an underline.
+     */
     public void underline(boolean underlineEnabled) {
         underline = underlineEnabled;
     }
 
+    /**
+     * Enable text underline.
+     */
     public void underline() {
         underline(true);
     }
 
+    /**
+     * Set the background color of the text with a hex value.
+     * @param hexColor The hex color code to use.
+     */
     public void bgColor(String hexColor) throws InvalidRGBAlpha, InvalidHexCode, InvalidRGBValues {
         String hexBGMatch = ColorUtil.findClosestColor(hexColor, ColorsCLI256.getAllColors());
         bgColor = ColorsCLI256.getCode(hexBGMatch);
     }
 
+    /**
+     * Set the background color of the text with an RGB value.
+     * @param red The red value of the color.
+     * @param green The green value of the color.
+     * @param blue The blue value of the color.
+     */
     public void bgColor(Integer red, Integer green, Integer blue) throws InvalidHexCode, InvalidRGBAlpha, InvalidRGBValues {
         Integer[] rgbMatch = ColorUtil.findClosestColor(
                 red,
@@ -52,11 +91,20 @@ public class AnsiChain {
         bgColor = ColorsCLI256.getCode(rgbMatch[0], rgbMatch[1], rgbMatch[2]);
     }
 
+    /**
+     * Set the text color with a hex value.
+     * @param hexColor The hex color code to use.
+     */
     public void color(String hexColor) throws InvalidRGBAlpha, InvalidHexCode, InvalidRGBValues {
         String hexBGMatch = ColorUtil.findClosestColor(hexColor, ColorsCLI256.getAllColors());
         fgColor = ColorsCLI256.getCode(hexBGMatch);
     }
 
+    /**
+     * Set the text color with an RGB value.
+     * @param red The red value of the color.
+     * @param green The green value of the color.
+     */
     public void color(Integer red, Integer green, Integer blue) throws InvalidHexCode, InvalidRGBAlpha, InvalidRGBValues {
         Integer[] rgbMatch = ColorUtil.findClosestColor(
                 red,
@@ -68,6 +116,11 @@ public class AnsiChain {
         fgColor = ColorsCLI256.getCode(rgbMatch[0], rgbMatch[1], rgbMatch[2]);
     }
 
+    /**
+     * Apply the current text styling properties to the text.
+     * @param inputText The text to apply the styling to.
+     * @return The styled text for the terminal.
+     */
     public String apply(String inputText) {
         return (fgColor != null
                     ? "\u001b[38;5;" + fgColor + "m"
@@ -83,6 +136,10 @@ public class AnsiChain {
                 )
                 + (underline
                     ? "\u001b[4m"
+                    : ""
+                )
+                + (italic
+                    ? "\u001b[3m"
                     : ""
                 )
                 + inputText + "\u001b[0m";
