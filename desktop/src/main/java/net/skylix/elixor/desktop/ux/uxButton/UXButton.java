c17624b9212d7aa432b949ea0d5a1a73c4f544ae
Rayyan (r - eye - an) <xfaonae@protonmail.com>
2022-04-27 07:22:07 -0400

    [Fix desktop] Button overlaying issue

--- desktop/src/main/java/net/skylix/elixor/desktop/ux/uxButton/UXButton.java
+++ desktop/src/main/java/net/skylix/elixor/desktop/ux/uxButton/UXButton.java
@@ -6,6 +6,7 @@ import net.skylix.elixor.terminal.color.errors.InvalidHexCode;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
 
 public class UXButton extends UXComponent {
     public UXButton(String label) throws InvalidHexCode {
@@ -20,14 +21,13 @@ public class UXButton extends UXComponent {
         public Element(String value) {
             super();
 
-            JLabel text = new JLabel(value);
+            final JLabel text = new JLabel(value);
+            final Dimension size = new Dimension(100, 30);
+
             text.setForeground(new java.awt.Color(255, 255, 255));
 
             setLayout(new GridBagLayout());
             add(text);
-
-            Dimension size = new Dimension(100, 30);
-
             setPreferredSize(size);
 
             addMouseListener(new java.awt.event.MouseAdapter() {
@@ -65,24 +65,30 @@ public class UXButton extends UXComponent {
 
         @Override
         protected void paintComponent(Graphics g) {
-            Graphics2D g2d = (Graphics2D) g;
+            final Graphics2D g2d = (Graphics2D) g;
+            final BufferedImage img = new BufferedImage(getWidth(), getHeight(), BufferedImage.TYPE_INT_ARGB);
+            final Graphics2D g2dImg = img.createGraphics();
 
-            g2d.setClip(null);
-            g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-            g2d.setRenderingHint(RenderingHints.KEY_STROKE_CONTROL, RenderingHints.VALUE_STROKE_PURE);
+            g2dImg.setClip(null);
+            g2dImg.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+            g2dImg.setRenderingHint(RenderingHints.KEY_STROKE_CONTROL, RenderingHints.VALUE_STROKE_PURE);
 
             if (!isMouseDown && !isMouseOver) {
-                g2d.setColor(theme.getThemeAttribute("layerSolid2").getAwtColor());
-                System.out.println("Rgba(" + theme.getThemeAttribute("layerSolid2").getRed() + ", " + theme.getThemeAttribute("layerSolid2").getGreen() + ", " + theme.getThemeAttribute("layerSolid2").getBlue() + ", " + theme.getThemeAttribute("layerSolid2").getAlpha() + ")");
+                g2dImg.setColor(theme.getThemeAttribute("dynamic1").getAwtColor());
             } else {
-                g2d.setColor(theme.getThemeAttribute("layerSolid3").getAwtColor());
-                System.out.println("Rgba(" + theme.getThemeAttribute("layerSolid2").getRed() + ", " + theme.getThemeAttribute("layerSolid2").getGreen() + ", " + theme.getThemeAttribute("layerSolid2").getBlue() + ", " + theme.getThemeAttribute("layerSolid2").getAlpha() + ")");
+                g2dImg.setColor(theme.getThemeAttribute("dynamic2").getAwtColor());
             }
 
-            int radius = 6;
-            g2d.fillRoundRect(0, 0, getWidth(), getHeight(), radius, radius);
+            final int radius = 6;
 
-            g2d.setColor(new Color(0, 0, 0, 0));
+            g2dImg.fillRoundRect(0, 0, getWidth(), getHeight(), radius, radius);
+            g2dImg.setColor(new Color(0, 0, 0, 0));
+
+            // clear first
+            g2d.fillRect(0, 0, getWidth(), getHeight());
+
+            // apply g2dImg to g2d
+            g2d.drawImage(img, 0, 0, null);
         }
     }
 }
