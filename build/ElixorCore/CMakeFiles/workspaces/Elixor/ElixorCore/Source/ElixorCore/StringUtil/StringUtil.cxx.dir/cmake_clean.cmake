9c2201776c9789f1848778f8f79d2b80d46418a9
XFaon <xfaonae@protonmail.com>
2022-05-27 15:08:28 +0000

    build: Removed build files

--- build/ElixorCore/CMakeFiles/workspaces/Elixor/ElixorCore/Source/ElixorCore/StringUtil/StringUtil.cxx.dir/cmake_clean.cmake
+++
@@ -1,5 +0,0 @@
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles//workspaces/Elixor/ElixorCore/Source/ElixorCore/StringUtil/StringUtil.cxx.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/ElixorCore/CMakeFiles/workspaces/Elixor/ElixorCore/Source/ElixorCore/StringUtil/StringUtil.cxx.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for /workspaces/Elixor/ElixorCore/Source/ElixorCore/StringUtil/StringUtil.cxx.
-# This may be replaced when dependencies are built.
