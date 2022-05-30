27c568fafaed64c5771ce9702872e314e6ad16f9
Rayyan (r - eye - an) <xfaonae@protonmail.com>
2022-05-30 18:34:00 -0400

    feat: Vulkan find lib

--- ElixorRuntime/ElixorRuntime/ElixorRuntime.cxx
+++ ElixorRuntime/ElixorRuntime/ElixorRuntime.cxx
@@ -1,8 +1,27 @@
 #include <iostream>
-// import vulca
+#include <GLFW/glfw3.h>
+#include <vulkan/vulkan.h>
+#include <thread>
+#include <chrono>
+#include <vector>
+
+using namespace std;
 
 int main(int argc, char ** argv) {
-    std::cout << "Hello, World!" << std::endl;
+    // Simple vulkan example
+
+    glfwInit();
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+    GLFWwindow * window = glfwCreateWindow(800, 600, "Vulkan", nullptr, nullptr);
+
+    // Create the msot minimal vulak instance
+    VkInstance instance;
+    VkResult result = vkCreateInstance(nullptr, nullptr, &instance);
+    if (result != VK_SUCCESS) {
+        cout << "Failed to create vulkan instance" << endl;
+        return -1;
+    }
 
     return 0;
 }
