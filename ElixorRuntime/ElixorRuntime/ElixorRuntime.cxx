3793bf3d7e96364934604b65624d9ad11e2c74df
Rayyan (r - eye - an) <xfaonae@protonmail.com>
2022-05-31 00:17:38 -0400

    feat: Vulkan find lib works

--- ElixorRuntime/ElixorRuntime/ElixorRuntime.cxx
+++ ElixorRuntime/ElixorRuntime/ElixorRuntime.cxx
@@ -15,12 +15,50 @@ int main(int argc, char ** argv) {
     glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
     GLFWwindow * window = glfwCreateWindow(800, 600, "Vulkan", nullptr, nullptr);
 
-    // Create the msot minimal vulak instance
+    VkApplicationInfo appInfo{};
+    appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    appInfo.pApplicationName = "Hello Triangle";
+    appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.pEngineName = "No Engine";
+    appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.apiVersion = VK_API_VERSION_1_0;
+
+    VkInstanceCreateInfo createInfo{};
+    createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    createInfo.pApplicationInfo = &appInfo;
+
+    uint32_t glfwExtensionCount = 0;
+    const char** glfwExtensions;
+
+    glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+    createInfo.enabledExtensionCount = glfwExtensionCount;
+    createInfo.ppEnabledExtensionNames = glfwExtensions;
+
+    createInfo.enabledLayerCount = 0;
+
     VkInstance instance;
-    VkResult result = vkCreateInstance(nullptr, nullptr, &instance);
-    if (result != VK_SUCCESS) {
-        cout << "Failed to create vulkan instance" << endl;
-        return -1;
+
+    VkResult result = vkCreateInstance(&createInfo, nullptr, &instance);
+
+    if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
+        throw std::runtime_error("failed to create instance!");
+    }
+
+    // Create the msot minimal vulak instance
+//    VkInstance instance;
+//    VkResult result = vkCreateInstance(nullptr, nullptr, &instance);
+//    if (result != VK_SUCCESS) {
+//        cout << "Failed to create vulkan instance" << endl;
+//        return -1;
+//    }
+
+    glfwShowWindow(window);
+    // Set resizable with hint
+    glfwSetWindowAttrib(window, GLFW_RESIZABLE, GLFW_TRUE);
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
     }
 
     return 0;
