9e783e52b6d682ea702a330bb8d2aefcdfa88558
Joseph Maldjian <josephavm@icloud.com>
2022-07-07 20:33:52 -0500

    Boom
    Co-authored-by: XFaon <xfaonae@protonmail.com>

--- scratch/main.cxx
+++ scratch/main.cxx
@@ -2,9 +2,11 @@
 #include <iostream>
 
 using namespace Elixor::Server::Linux;
+using namespace Elixor::Server::MacOS;
 using namespace Elixor::Internal;
 using namespace std;
 
 int main() {
     LinuxSocket socket;
+    MacOSSocket mac;
 }
