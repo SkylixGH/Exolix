a09584966950d3e30f02136f078e74d62c88cde6
xfaon <xfaonae@protonmail.com>
2022-08-02 12:15:35 -0400

    Removed C++ files

--- script/lib/meta.js
+++
@@ -1,8 +0,0 @@
-import * as path from "path";
-import {fileURLToPath} from "url";
-
-const fileName = fileURLToPath(import.meta.url);
-const dirName = path.dirname(fileName);
-const projectRoot = path.join(dirName, "../../");
-
-export {projectRoot}
