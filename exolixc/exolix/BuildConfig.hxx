771ebaa2bcfdd5bbb5c2a27c0f89c8eb74f27bbb
xfaon <xfaonae@protonmail.com>
2022-08-08 01:33:55 -0400

    partial: Chunk 2.x

---
+++ exolixc/exolix/BuildConfig.hxx
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <string>
+
+namespace skylix::exolix::exolix {
+    class BuildConfig {
+    public:
+        const std::string llcMinVersion = "14.0.6";
+        const std::string lldMinVersion = "14.0.6";
+    };
+}
