5bcc2341a903697d9900aa70a80ba5fe9d699216
xfaon <xfaonae@protonmail.com>
2022-08-23 22:13:29 -0400

    feat: Updated HTTP again??!!!

--- src/http/http.h
+++ src/http/http.h
@@ -2,11 +2,26 @@
 
 #include <exolix/net.h>
 #include <exolix/err.h>
+#include <functional>
 
 namespace exolix::http {
+    class HttpHeaders {
+
+    };
+
+    class HttpRequest {
+
+    };
+
+    class HttpResponse {
+
+    };
+
     class HttpServer : public net::SocketServer {
     private:
+        std::function<void(HttpRequest &, HttpResponse &)> onHttpRequest = [] (HttpRequest &request, HttpResponse &response) {};
 
     public:
+
     };
 }
