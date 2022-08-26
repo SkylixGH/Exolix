f0972bb81a30866a892a4400920e21c8443296e3
Rayyan (r - eye - an) <xfaonae@protonmail.com>
2022-08-26 13:28:38 -0400

    feat: Added RESULT to prevent using exceptions

--- lib/error/result.hxx
+++ lib/error/result.hxx
@@ -1,5 +1,47 @@
 #pragma once
 
-namespace exolix {
+#include <optional>
+#include <iostream>
+#include <exception>
 
+namespace exolix {
+    template <typename Keys>
+    class Error {
+    private:
+        Keys reason;
+
+    public:
+        explicit Error(Keys keyReason):
+            reason(keyReason) {
+        }
+    };
+
+    template <typename SafeReturn, typename ErrorKeys>
+    class Result {
+    private:
+        SafeReturn value {};
+        Error<ErrorKeys> error { 0 };
+
+        void renderError() {
+            std::cout << "An error occurred ";
+        }
+
+    public:
+        const bool hasError;
+
+        explicit Result(SafeReturn returnData):
+            value(returnData), hasError(false) {}
+
+        explicit Result(Error<ErrorKeys> errorData):
+            error(errorData), hasError(true) {}
+
+        SafeReturn get() {
+            if (hasError) {
+                renderError();
+                throw std::exception("Program will now terminate with exit code (1)");
+            }
+
+            return value;
+        }
+    };
 }
--- test/main/main.cxx
+++ test/main/main.cxx
@@ -1,6 +1,16 @@
 #include <exolix.hxx>
 
+using namespace exolix;
+
+Result<std::string, int> getThing() {
+    return Result<std::string, int>(Error(1));
+}
+
 int main() {
-    exolix::test();
+    auto x = getThing();
+
+    auto y = x.get();
+
+    std::cout << y << "\n";
     return 0;
 }
