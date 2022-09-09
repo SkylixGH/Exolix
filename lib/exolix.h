ae7ec56c7797216ec4238af3685b89f3d61fd9d3
xfaon <xfaonae@protonmail.com>
2022-09-09 17:13:56 -0400

    Wiki(number): Added documentation for primitive number types

--- lib/exolix.h
+++ lib/exolix.h
@@ -4,11 +4,4 @@
  * Exolix Software Framework
  */
 namespace exolix {
-    // Global Definitions
-
-    /**
-     * A success constant that can be returned from a function
-     * when there is no error.
-     */
-    const int Success = -1;
 }
