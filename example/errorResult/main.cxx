0e2caa617f1d55a41f782c084b61b806d6699e99
xfaon <xfaonae@protonmail.com>
2022-09-14 22:13:59 -0400

    Feat(result): Added Result<T, E> type similar to rust

--- example/errorResult/main.cxx
+++ example/errorResult/main.cxx
@@ -1,5 +1,24 @@
-#include "exolix"
+#include "exolixs"
+#include <string>
+
+using namespace ex;
+
+Result<std::string, int> work(const int &number) {
+    if (number % 2 == 0) {
+        return Ok<std::string>("Even");
+    } else {
+        return Err<int>(number);
+    }
+}
 
 int main() {
-    hi();
+    Result<std::string, int> res = work(5);
+
+    if (!res.isError) {
+        std::cout << "Result: " << res.get() << std::endl;
+    } else {
+        std::cout << "Error: " << res.getError() << std::endl;
+    }
+
+    return 0;
 }
--- lib/result/result.hxx
+++ lib/result/result.hxx
@@ -1,5 +1,65 @@
 #pragma once
 
-namespace exolix {
+#include <iostream>
+#include <optional>
 
+namespace exolix {
+    template <typename ErrorType>
+    class Err {
+    public:
+        const ErrorType error;
+
+        explicit Err(ErrorType error) : error(error) {};
+    };
+
+    template <typename ValueType>
+    class Ok {
+    public:
+        const ValueType value;
+
+        explicit Ok(ValueType value) : value(value) {};
+    };
+
+    template <typename ValueType, typename ErrorType>
+    class Result {
+    private:
+//
+//        std::optional<ValueType> value;
+//        std::optional<ErrorType> error;
+
+        union {
+            ValueType value;
+            ErrorType error;
+        };
+
+    public:
+        const bool isError;
+
+        Result(Ok<ValueType> value) : value(value.value), isError(false) {}
+
+        Result(Err<ErrorType> error) : error(error.error), isError(true) {}
+
+        ~Result() {
+            if (isError) {
+                error.~ErrorType();
+            } else {
+                value.~ValueType();
+            }
+        }
+
+        ValueType get() {
+            if (isError) {
+                throw std::runtime_error("Result has error");
+            }
+
+            return value;
+        }
+
+        ErrorType getError() {
+            if (!isError)
+                throw std::runtime_error("Result has no error");
+
+            return error;
+        }
+    };
 }
