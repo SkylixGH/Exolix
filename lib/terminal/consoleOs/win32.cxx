79ffcf655aaefc67c2dd8d125a0d0cc247a26823
xfaonae <xfaonae@protonmail.com>
2022-09-16 18:41:43 -0400

    Unf Feat(*): Added dead win32 console listener

--- lib/terminal/consoleOs/win32.cxx
+++ lib/terminal/consoleOs/win32.cxx
@@ -1,11 +1,20 @@
 #include "win32.hxx"
 
+#ifdef _WIN32
+    #include <windows.h>
+    #include <iostream>
+#endif
+
 namespace exolix {
     void TerminalConsoleWin32Impl::listener() {
         printf("Listener started for win32\n");
 
         while (!isDisposed()) {
+            DriverKeyboardEvent event {};
 
+#ifdef _WIN32
+            // Wait for key
+#endif
         }
     }
 }
